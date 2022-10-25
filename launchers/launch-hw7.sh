38a4df41c564b0617628bacc56d6a20466370e7e
Matthew Bedard <matthew_bedard@student.uml.edu>
2022-10-25 13:14:23 -0400

    finished

--- launchers/launch-hw7.sh
+++ launchers/launch-hw7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HW_PACKAGE="homework7"
+HW_PACKAGE="hw7pkg"
 HW_LAUNCH="hw7.launch"
 
 source /environment.sh
--- packages/hw7pkg/src/cvnode.py
+++ packages/hw7pkg/src/cvnode.py
@@ -5,22 +5,36 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-class ImageFlippa:
+class ImageCropper:
     def __init__(self):
-        rospy.Subscriber("image", Image, self.flippa_cb)
-        self.pub = rospy.Publisher("flipped", Image, queue_size = 10)
+        rospy.Subscriber("/image", Image, self.cropper_cb)
 
+        self.pub = rospy.Publisher("/image_cropped", Image, queue_size = 10)
+        self.pub_white = rospy.Publisher("/image_white", Image, queue_size = 10)
+        self.pub_yellow = rospy.Publisher("/image_yellow", Image, queue_size = 10)
         self.bridge = CvBridge()
-    def flippa_cb(self, msg):
-        cv_img - self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-        cv_flipped = cv2.flip(cv_img, 0)
+    def cropper_cb(self, msg):
+        cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        cv_cropped = cv_img[240:480,0:640]
+        ros_cropped = self.bridge.cv2_to_imgmsg(cv_cropped, "bgr8")
+        self.pub.publish(ros_cropped)
+        
+        image_hsv = cv2.cvtColor(cv_cropped, cv2.COLOR_BGR2HSV)
+        image_white = cv2.inRange(image_hsv,(30,0,155),(255,100,255))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+        white_erode = cv2.erode(image_white, kernel)
+        white_dilate = cv2.dilate(white_erode, kernel)
+        output_white = self.bridge.cv2_to_imgmsg(white_dilate, "mono8")
+        self.pub_white.publish(output_white)
 
-        ros_flipped = self.bridge.cv2_to_imgmsg(cv_flipped, "bgr8")
-
-        self.pub.publish(ros_flipped)
+        image_yellow = cv2.inRange(image_hsv,(25,100,100),(50,255,255))
+        yellow_erode = cv2.erode(image_yellow, kernel)
+        yellow_dilate = cv2.dilate(yellow_erode, kernel)
+        ros_yellow = self.bridge.cv2_to_imgmsg(yellow_dilate, "mono8")
+        self.pub_yellow.publish(ros_yellow)
 
 if __name__ == "__main__":
-    rospy.init_node("image_flipper", anonymous = True)
-    img_flip = ImageFlippa()
+    rospy.init_node("image_cropper", anonymous = True)
+    img_crop = ImageCropper()
     rospy.spin()
