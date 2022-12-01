9d8615f1e751477435de1fe3ba40331670a76f0e
Matthew Bedard <matthew_bedard@student.uml.edu>
2022-12-01 18:33:04 -0500

    hw10

--- packages/hw9pkg/src/hw10node.py
+++ packages/hw9pkg/src/hw10node.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3 
 
 import rospy
-from example_service.srv import *
-
-def fib_client(x):
-    rospy.wait_for_service('calc_fibonacci')
-    try:
-        calc_fibonacci = rospy.ServiceProxy('calc_fibonacci', Fibonacci)
-        resp1 = calc_fibonacci(x)
-        return resp1.sequence
-    except (rospy.ServiceException) as e:
-        print("Service call failed: %s" %e)
+import actionlib
+import example_action_server.msg
+from example_service.srv import Fibonacci
 
 if __name__=="__main__":
-    fib_client(3)
-    fib_client(15)
+    rospy.init_node("hw10_node")
+    rate = rospy.Rate(1)
+    rate.sleep()
+    rospy.loginfo("node initiated")
+
+    rospy.wait_for_service("/calc_fibonacci")
+    calc_fib = rospy.ServiceProxy("/calc_fibonacci", Fibonacci)
+    
+    rospy.loginfo("SERVICE: call calc_fib order 3")
+    resp1 = calc_fib(3)
+    rospy.loginfo(resp1.sequence)
+      
+    rospy.loginfo("SERVICE: call calc_fib order 15")
+    resp1 = calc_fib(15)
+    rospy.loginfo(resp1.sequence)
+
+
+    client = actionlib.SimpleActionClient('fibonacci', example_action_server.msg.FibonacciAction)
+    client.wait_for_server()
+    
+    goal = example_action_server.msg.FibonacciGoal(order=3)
+    client.send_goal(goal)
+    client.wait_for_result()
+    rospy.loginfo(client.get_result())  # A FibonacciResult
+
+    goal = example_action_server.msg.FibonacciGoal(order=15)
+    client.send_goal(goal)
+    client.wait_for_result()
+    rospy.loginfo(client.get_result())  # A FibonacciResult
